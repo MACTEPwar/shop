7b27308cc4d9c8a4440f724cf856618d4d885142
Виталий Шебаниц <mactepwar@outlook.com>
2019-03-02 23:30:23 +0200

    Первый коммит. Так же добавил предположительные модели и их поля

---
+++ Shop_api/Shop_api/Data/dataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop_api.Models;
+
+namespace Shop_api.Data
+{
+    public class dataContext : IdentityDbContext<User>
+    {
+        public dataContext(DbContextOptions<dataContext> options) : base(options) { }
+    }
+
+
+}
---
+++ Shop_api/Shop_api/Models/UserMassage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_api.Models
+{
+    public class UserMassage
+    {
+        // ид
+        // ид пользователя
+        // ид сообщения
+        // время отправления
+        // время получения
+        // время прочтения
+    }
+}
